aa9c4fdfdaca42bb092048ff84cc0ea5066d88d6
daniil-b2be <dan.developer@born2be.media>
2025-11-02 20:52:59 +0200

    page

--- src/features/contact-form/ui/thank-you.tsx
+++ src/features/contact-form/ui/thank-you.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
 import { Link } from '@/i18n/navigation';
 
@@ -8,14 +9,19 @@ import { Button } from '@/shared/ui/kit/button';
 import { Title } from '@/shared/ui/kit/title';
 
 export const ThankYou = () => {
+  const t = useTranslations('thankYou');
+
   return (
     <section className="relative flex h-[350px] flex-col justify-center gap-10 overflow-hidden p-10">
       <Title as="h5" size="3xl" className="max-md:text-2xl">
-        Thank you for reaching out to Renvex. <br />
-        Our team will contact you shortly.
+        {t('title.0', { fallback: 'Thank you for reaching out to Renvex.' })}{' '}
+        <br />
+        {t('title.1', { fallback: 'Our team will contact you shortly.' })}
       </Title>
       <Link href="/">
-        <Button variant="primary">Return to home page</Button>
+        <Button variant="primary">
+          {t('button', { fallback: 'Return to home page' })}
+        </Button>
       </Link>
       <Image
         className="absolute -right-10 bottom-0 h-[320px] w-[626px] max-md:hidden"
